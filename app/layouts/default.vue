020eb9522e9aea8bfbcccbd74b52a7c74ecf0204
stefanBid <biddau.stefano99@gmail.com>
2025-11-19 23:46:37 +0100

    fix: update class name for transition effect in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
 <template>
   <div>
     <main class="pt-16 px-6 md:px-10">
-      <div class="w-full max-w-[1400px] mx-auto u-sb-soft-transition">
+      <div class="w-full max-w-[1400px] mx-auto u-app-soft-transition">
         <slot></slot>
       </div>
     </main>
