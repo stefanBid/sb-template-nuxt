310db3d7c8780325b951365b09430d14416a31ce
stefanBid <biddau.stefano99@gmail.com>
2025-11-19 23:12:05 +0100

    feat: aggiungere configurazione di stili e transizioni con Tailwind CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
   vite: {
     plugins: [
