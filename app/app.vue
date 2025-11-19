e79eaa37d630fa62c3ff130fd6589189576c1ab8
stefanBid <biddau.stefano99@gmail.com>
2025-11-19 23:35:56 +0100

    feat: add project structure with layout and main template

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,3 @@
 <template>
-  <div>
-    <h1 class=" ty-app-impact text-app-contrast h-[200vh]">
-      Welcome to My Nuxt App
-    </h1>
-  </div>
+  <NuxtLayout />
 </template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <main class="pt-16 px-6 md:px-10">
+      <div class="w-full max-w-[1400px] mx-auto u-sb-soft-transition">
+        <slot></slot>
+      </div>
+    </main>
+  </div>
+</template>
