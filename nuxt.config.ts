ecfc7aff691eca6908ef8b9ca3ebe187a646c6c2
stefanBid <biddau.stefano99@gmail.com>
2025-11-19 22:37:07 +0100

    feat: update app.vue formatting and enhance package.json with linting scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/image'],
-    eslint: {
+  eslint: {
     config: {
       stylistic: true,
 
